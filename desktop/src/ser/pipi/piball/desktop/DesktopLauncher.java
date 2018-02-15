0320eef9ffb0a73b7e45e05a632a67a7c0f448e0
Evgeny Chormonov <en.chormonov@gmail.com>
2018-02-15 11:07:44 +0300

    init

---
+++ desktop/src/ser/pipi/piball/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ser.pipi.piball.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ser.pipi.piball.piball;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new piball(), config);
+	}
+}
