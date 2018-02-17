6f84d539b3d68f6c4bb9d3e719f58a678f8abbee
Evgeny Chormonov <en.chormonov@gmail.com>
2018-02-18 00:57:16 +0300

    IMPR: android portrait customization

--- android/src/ser/pipi/piball/AndroidLauncher.java
+++ android/src/ser/pipi/piball/AndroidLauncher.java
@@ -2,6 +2,7 @@ package ser.pipi.piball;
 
 import android.os.Bundle;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import ser.pipi.piball.piball;
@@ -11,6 +12,10 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useAccelerometer = false;
+		config.useCompass = false;
+		config.useWakelock = true;
+		config.hideStatusBar = true;
 		initialize(new piball(), config);
 	}
 }
