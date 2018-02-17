994925544a9c0a55ac707ad1261ac916b319f49e
Evgeny Chormonov <en.chormonov@gmail.com>
2018-02-17 18:50:54 +0300

    IMPR: add run configuration

---
+++ core/src/ser/pipi/piball/Platform.java
@@ -0,0 +1,11 @@
+package ser.pipi.piball;
+
+/**
+ * Created by ser on 15.02.18.
+ */
+
+public class Platform {
+
+    static public final int screen_width = 800;
+    static public final int screen_heigth = 900;
+}
--- desktop/src/ser/pipi/piball/desktop/DesktopLauncher.java
+++ desktop/src/ser/pipi/piball/desktop/DesktopLauncher.java
@@ -2,11 +2,17 @@ package ser.pipi.piball.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import ser.pipi.piball.Platform;
 import ser.pipi.piball.piball;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "ball";
+		config.width = Platform.screen_width;
+		config.height = Platform.screen_heigth;
+		config.resizable = false;
 		new LwjglApplication(new piball(), config);
 	}
 }
