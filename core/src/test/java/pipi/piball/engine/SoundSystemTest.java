df06fab1fe12ca9ea91f952a4b2060b895e209ad
Evgeny Chormonov <en.chormonov@gmail.com>
2018-03-01 00:26:51 +0300

    TEST: sound system methods

---
+++ core/src/test/java/pipi/piball/engine/SoundSystemTest.java
@@ -0,0 +1,38 @@
+package pipi.piball.engine;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import ser.pipi.piball.engine.SoundSystem;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.*;
+
+/**
+ * Created by ser on 27.02.18.
+ */
+public class SoundSystemTest {
+    @Test
+    public void appendSound() throws Exception {
+        ArrayList<String> test = new ArrayList<String>(
+                Arrays.asList("a", "b"));
+        SoundSystem.appendSound(test,"a");
+        Assert.assertEquals(test.size(),2);
+
+        SoundSystem.appendSound(test,"c");
+        Assert.assertEquals(test.size(),3);
+
+        SoundSystem.appendSound(test,"c");
+        Assert.assertEquals(test.size(),3);
+
+        SoundSystem.removeSound(test,"c");
+        Assert.assertEquals(test.size(),2);
+
+        SoundSystem.removeSound(test,"c");
+        Assert.assertEquals(test.size(),2);
+    }
+
+}
