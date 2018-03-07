9253d8e943aa56e32fe2377a6a812130bfaeb6c3
Evgeny Chormonov <en.chormonov@gmail.com>
2018-03-07 03:05:55 +0300

    FEAT: add json custom serializer

---
+++ core/src/java/ser/pipi/piball/net/CustJsonSerializer.java
@@ -0,0 +1,41 @@
+package ser.pipi.piball.net;
+
+import com.badlogic.gdx.Gdx;
+import com.esotericsoftware.jsonbeans.Json;
+import com.esotericsoftware.jsonbeans.JsonException;
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+
+import ser.pipi.piball.engine.StateStore;
+
+/**
+ * Created by ser on 06.03.18.
+ */
+
+class CustJsonSerializer<T> extends Serializer<T> {
+
+    final Json json;
+    final Class<T> type;
+
+    public CustJsonSerializer(Class<T> type) {
+        super(false, false);
+        this.json = new Json();
+        this.type = type;
+    }
+
+    @Override
+    public T read(Kryo kryo, Input input, Class type) {
+        final String js = input.readString();
+        //System.out.println("read: " + js);
+        return json.fromJson(this.type,js);
+    }
+
+    @Override
+    public void write(Kryo kryo, Output output, T object) {
+            final String js = json.toJson(object, type);
+            //System.out.println("write " + js);
+            output.writeString(js);
+    }
+}
