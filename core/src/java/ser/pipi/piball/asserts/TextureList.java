a46be8e65f7042394b61f99bce3f23bd2ff27724
Evgeny Chormonov <en.chormonov@gmail.com>
2018-03-15 20:04:01 +0300

    FEAT: add loadtexture to tesxture list class

--- core/src/java/ser/pipi/piball/asserts/TextureList.java
+++ core/src/java/ser/pipi/piball/asserts/TextureList.java
@@ -1,9 +1,16 @@
 package ser.pipi.piball.asserts;
 
+import com.badlogic.gdx.graphics.Texture;
+
 /**
  * Created by ser on 10.03.18.
  */
 
 public class TextureList {
     public static final String FINAL_LOGO = "final_logo.png";
+    public static final String WELCOME = "field.png";
+
+    public static Texture loadTexture(String name){
+        return new Texture(name);
+    }
 }
