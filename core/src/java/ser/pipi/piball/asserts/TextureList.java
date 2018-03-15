ed73dfb8f446fbb573e9db05b664340e46ca8148
Evgeny Chormonov <en.chormonov@gmail.com>
2018-03-16 02:59:48 +0300

    FEAT: add Texture to texture List : ball, fielt result...

--- core/src/java/ser/pipi/piball/asserts/TextureList.java
+++ core/src/java/ser/pipi/piball/asserts/TextureList.java
@@ -7,8 +7,14 @@ import com.badlogic.gdx.graphics.Texture;
  */
 
 public class TextureList {
-    public static final String FINAL_LOGO = "final_logo.png";
+    //public static final String RESULT_FON = "final_logo.png";
+    public static final String RESULT_FON = "result_w_s.png";
     public static final String WELCOME = "green_welcome_wr3.png";
+    //public static final String ARENA = "field.png";
+    public static final String ARENA = "field_simple_score.png";
+    public static final String BALL = "big.png";
+    public static final String PADDLE_ENEMY = "paddle_enemy.png";
+    public static final String PADDLE_SELF = "paddle_self.png";
 
     public static Texture loadTexture(String name){
         return new Texture(name);
