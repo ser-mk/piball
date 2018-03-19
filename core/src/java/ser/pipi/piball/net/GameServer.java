ee9487966371da6f814b8234c9b73d7875f75a22
Evgeny Chormonov <en.chormonov@gmail.com>
2018-03-19 22:03:03 +0300

    FIX: app fail when bind exception and don't print network expection

--- core/src/java/ser/pipi/piball/net/GameServer.java
+++ core/src/java/ser/pipi/piball/net/GameServer.java
@@ -2,8 +2,6 @@ package ser.pipi.piball.net;
 
 import com.esotericsoftware.kryonet.Server;
 
-import java.io.IOException;
-
 import ser.pipi.piball.SettingsStruct;
 
 import ser.pipi.piball.net.Network.ConnectionState;
@@ -36,18 +34,20 @@ public class GameServer extends NetworkBaseClass {
             gameServer.bind(Network.portTCP(bankPort), Network.portUDP(bankPort));
 
             state = ConnectionState.WAIT_PLAYER;
-
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
             state = ConnectionState.NETWORK_EXCEPTION;
         }
 
     }
 
-    ConnectionState state = ConnectionState.WAIT_PLAYER;
-
     @Override
     public boolean waitPlayer(float delta) {
+
+        if (state == ConnectionState.NETWORK_EXCEPTION){
+            return true;
+        }
+
         final int qtyConnection = gameServer.getConnections().length;
         if(qtyConnection == 1){
             state = ConnectionState.CONNECTED_PLAYER;
