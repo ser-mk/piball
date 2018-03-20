6b3bf0606a42f4f46b87d125c486d9e77b56f4ce
Evgeny Chormonov <en.chormonov@gmail.com>
2018-03-20 03:59:34 +0300

    IMPR: change color clear for welcome screen

--- core/src/java/ser/pipi/piball/Welcome.java
+++ core/src/java/ser/pipi/piball/Welcome.java
@@ -150,7 +150,7 @@ class Welcome implements Screen {
     }
 
     private void render(){
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 1, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         spriteBatch.begin();
         spriteBatch.draw(welcomeTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
